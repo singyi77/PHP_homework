3407c8df411377b6bff6eacc3e4722d763c1e4b3
singyi <113578837+singyi77@users.noreply.github.com>
2023-02-17 16:01:38 +0800

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>This is a page.</title>
+    </head>
+    <body>
+        Hello HTML
+        <BR>
+    <?php
+    echo "Hello PHP";
+    ?>
+    </body>
+</html>
